54ee03bf1b9f5e48d06913d59959716358979e65
Spradlin <cspradlin@gsiccorp.net>
2020-06-15 16:49:24 -0400

    Added Navigation and Footer

--- header.js
+++ header.js
@@ -1,4 +1,4 @@
-var slideIndex = 0;
+var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -13,9 +13,9 @@ function showSlides(n) {
   console.log(n);
   var i;
   var slides = document.getElementsByClassName("mySlideshow");
-  if (n > slides.length) {slideIndex = 0}    
+  if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
-  for (i = 1; i < slides.length; i++) {
+  for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
   
