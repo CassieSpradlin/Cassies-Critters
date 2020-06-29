d7350cc012281dcab0c337233a0b73c0955e72f6
Spradlin <cspradlin@gsiccorp.net>
2020-06-29 19:51:45 -0400

    Added new images, finished blogs and order form

--- header.js
+++ header.js
@@ -23,6 +23,21 @@ function showSlides(n) {
  
 }
 
+// var slideIndex = 0;
+// showSlides();
+
+// function showSlides() {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlideshow");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}
+//   slides[slideIndex-1].style.display = "block";
+//   setInterval(showSlides, 3000); // Change image every 2 seconds
+// }
+
 //Get the button:
 mybutton = document.getElementById("myBtn");
 
